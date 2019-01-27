312dcb0adacec6d0e47f8364b43044eee1a3dfda
takion <jo@surikat.pro>
2019-01-27 14:00:19 +0100

    wip

--- webpack.config.js
+++ webpack.config.js
@@ -2,33 +2,37 @@ const path = require('path');
 
 const config = {
   entry: __dirname + '/src/index.js',
-  devtool: 'inline-source-map',
   output: {
-    path: __dirname + '/lib',
-    filename: 'index.js',
+    filename: 'lib/[name].js',
     library: '@redux-up/hooks',
     libraryTarget: 'umd'
   },
   module: {
     rules: [
       {
-        test: /(\.js)$/,
+        test: /\.(js)?$/,
+        exclude: /node_modules/,
         loader: 'babel-loader',
-        exclude: /(node_modules)/
-      },
+        options: {
+          plugins: [
+
+          ],
+          presets: [
+
+          ],
+          cacheDirectory: true,
+          babelrc: true
+        }
+      }
     ]
-  },
-  resolve: {
-    modules: [path.resolve('./node_modules'), path.resolve('./src')],
-    extensions: ['.js']
   },
   externals: {
     react : {
       commonjs: 'react',
       commonjs2: 'react',
-      amd: 'react',
     },
   },
+  devtool: 'source-map',
 };
 
 module.exports = config;
